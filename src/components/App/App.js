abb73ed1462a03c8e274a4def1c21c7a0b27432b
Wolfy <alka-wolfik@yandex.ru>
2022-11-10 20:44:12 +0300

    added route for create recipes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import './App.scss';
 import { PostsList } from '../Post/PostList/PostsList';
 import SignIn from '../SignIn/SignIn';
 import SignUp from '../SignUp/SignUp';
 import Header from '../Header/Header';
-import { baseRoute, articleRoute, signInRoute, signUpRoute } from '../../constants';
+import { baseRoute, articleRoute, signInRoute, signUpRoute, newArticleRoute } from '../../constants';
+import CreateArticle  from '../Article/CreateArticle';
 
 function App() {
+    const useStateUser = () => {
+        const stateUserst = useSelector((state) => state.user);
+        return stateUserst;
+    };
+    const { userData } = useStateUser();
 
     return (
         <Router>
@@ -22,6 +29,7 @@ function App() {
                     <Route path={articleRoute} component={PostsList} exact />
                     <Route path={signInRoute} component={SignIn} exact />
                     <Route path={signUpRoute} component={SignUp} exact />
+                    <Route path={newArticleRoute} exact>{userData ? <CreateArticle /> : <Redirect to={'/sign-in'} />} </Route>
                 </main>
             </div>
         </Router>

6e6ddeb0dcc08ec3040e3a621ec2d46188104116
Wolfy <alka-wolfik@yandex.ru>
2022-11-12 04:42:28 +0300

    added route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+import { PostFull } from '../Post/PostFull/PostFull';
+import SearchRecipes from '../Search/SearchRecipes';
+import FilterRecipes from '../Filters/FilterRecipes';
 import './App.scss';
 import { PostsList } from '../Post/PostList/PostsList';
 import SignIn from '../SignIn/SignIn';
 import SignUp from '../SignUp/SignUp';
 import Header from '../Header/Header';
-import { baseRoute, articleRoute, signInRoute, signUpRoute, newArticleRoute } from '../../constants';
+import { baseRoute, articleRoute, signInRoute, signUpRoute, newArticleRoute, idRoute } from '../../constants';
 import CreateArticle  from '../Article/CreateArticle';
+import SortRecipes from '../Sort/SortRecipes';
 
 function App() {
     const useStateUser = () => {
@@ -26,7 +30,24 @@ function App() {
                 </header>
                 <main className='app_main'>
                     <Route path={baseRoute} exact render={() => <Redirect to='/articles' />} />
-                    <Route path={articleRoute} component={PostsList} exact />
+                    <Route path={articleRoute} render={() =>
+                        <>
+                            <SearchRecipes />
+                            <FilterRecipes />
+                            <SortRecipes />
+                            <PostsList />
+                        </>}
+                    exact />
+                    <Route
+                        path={`${articleRoute}${idRoute}`}
+                        exact
+                        render={({ match }) => {
+                            console.log('match.params = ', match.params);
+                            console.log('match.params.slug = ', match.params.id);
+                            const id = match.params.id;
+                            return <PostFull itemId={id} />;
+                        }}
+                    />
                     <Route path={signInRoute} component={SignIn} exact />
                     <Route path={signUpRoute} component={SignUp} exact />
                     <Route path={newArticleRoute} exact>{userData ? <CreateArticle /> : <Redirect to={'/sign-in'} />} </Route>

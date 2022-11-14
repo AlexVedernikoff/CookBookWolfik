eba959b8d27d6c2f7a3edf7203dffa1d6ea1302a
Wolfy <alka-wolfik@yandex.ru>
2022-11-14 08:56:01 +0300

    added form filters

--- src/components/Filters/FilterRecipes.js
+++ src/components/Filters/FilterRecipes.js
@@ -1,10 +1,23 @@
 import classes from './FilterRecipes.module.scss';
 
 const FilterRecipes = () => {
+
+    const Filters = ({ labelTxt, id }) => (
+        <div>
+            <input type={'checkbox'} id={id} className={classes['checkbox']} />
+            <label htmlFor={id}>{labelTxt}</label>
+        </div>
+
+    );
+
     return (
         <div className={classes['filters']} >
-filters
-        </div>
+            <h3>Фильтр по сложности приготовления</h3>
+            <form>
+                <Filters id='lite' labelTxt={'Легкий'} />
+                <Filters id='hard' labelTxt={'Сложный'} />
+            </form>
+        </div> 
     );
 };
 
